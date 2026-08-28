92b61c464a32ed963d7dca0a1e88d904f3d4545e
Yash-Raj-53526 <yashraj53526@gmail.com>
2026-08-28 20:47:57 +0530

    update

--- src/JavaCollectionFramework/Main.java
+++ src/JavaCollectionFramework/Main.java
@@ -2,11 +2,10 @@ package JavaCollectionFramework;
 
 import java.net.SocketTimeoutException;
 import java.util.*;
-
 import javax.swing.AbstractAction;
 
 public class Main {
-    public static void main(String[] args) { //This method has a constructor name 
+    public static void main(String[] args) { // This method has a constructor name
         ArrayList<Integer> list = new ArrayList<>();
         list.add(67);
         list.add(20);
@@ -19,16 +18,14 @@ public class Main {
         list.remove(1);
         System.out.println(list);
         System.out.println("list.size(): " + list.size());
-        
 
-
-        //Iterating through the list
+        // Iterating through the list
         System.out.println("Iterating through the list using for loop");
         Iterator<Integer> it = list.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             System.out.println(it.next());
         }
-        
+
         System.out.println("array list: " + list);
         Collections.sort(list);
         System.out.println("sorted list: " + list);
@@ -38,11 +35,9 @@ public class Main {
         // System.out.println("cloned list: " + clonedList);
 
         System.out.println("--------------------------------");
-        
 
-
-
-        //LinkedList -- collection of nodes, each node contains data and a reference to the next node
+        // LinkedList -- collection of nodes, each node contains data and a reference to
+        // the next node
         LinkedList<String> ll = new LinkedList<>();
         ll.add("Apple");
         ll.add("Banana");
@@ -51,19 +46,18 @@ public class Main {
         ll.addFirst("Grapes");
         System.out.println(ll);
         ll.addLast("Pineapple");
-        System.out.println(ll);   
+        System.out.println(ll);
 
-
-        //Iterating through the linked list
+        // Iterating through the linked list
         Iterator<String> it1 = ll.iterator();
-        while(it1.hasNext()){
+        while (it1.hasNext()) {
             System.out.println(it1.next());
         }
-
         System.out.println("--------------------------------");
 
-
-        //vector -- dynamic array, similar to ArrayList but synchronized means thread-safe i'e multiple threads can access it at the same time without any issues
+        // vector -- dynamic array, similar to ArrayList but synchronized means
+        // thread-safe i'e multiple threads can access it at the same time without any
+        // issues
         Vector<Double> v = new Vector<>();
         v.add(12.5);
         v.add(15.5);
@@ -72,8 +66,8 @@ public class Main {
 
         System.out.println("--------------------------------");
 
-
-        //Stack -- LIFO (Last In First Out) data structure, used to store data in a linear order, used in function calls, undo operations, etc.
+        // Stack -- LIFO (Last In First Out) data structure, used to store data in a
+        // linear order, used in function calls, undo operations, etc.
         Stack<String> stack = new Stack<>();
         stack.push("A");
         stack.push("B");
@@ -90,15 +84,16 @@ public class Main {
         stack.clear();
         System.out.println("cleared stack: " + stack);
 
-        //Iterating through the stack
-            Iterator<String> it2 = stack.iterator();
-            while(it2.hasNext()){
-                System.out.println(it2.next());
-            }
+        // Iterating through the stack
+        Iterator<String> it2 = stack.iterator();
+        while (it2.hasNext()) {
+            System.out.println(it2.next());
+        }
 
         System.out.println("--------------------------------");
 
-        //Queue -- FIFO (First In First Out) data structure, used to store data in a linear order, used in function calls, print jobs, etc.
+        // Queue -- FIFO (First In First Out) data structure, used to store data in a
+        // linear order, used in function calls, print jobs, etc.
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(90);
         queue.offer(23);
@@ -106,15 +101,15 @@ public class Main {
         queue.offer(456);
         queue.offer(2);
         System.out.println("queue: " + queue);
-        
+
         System.out.println("peeking " + queue.peek());
 
         System.out.println("Removing " + queue.poll());
 
-        System.out.println(queue);  
+        System.out.println(queue);
         System.out.println("-------------------------------------------");
 
-        //ArrayDeque
+        // ArrayDeque
         ArrayDeque<String> dq = new ArrayDeque<>();
         dq.offer("Yash");
         dq.offer("Tripti");
@@ -124,13 +119,13 @@ public class Main {
         // System.out.println("Peek: "+ dq.peek());
         // System.out.println("Poll: " + dq.poll());
         System.out.println("Itrating :- ");
-        //itration
+        // itration
         // Iterator<String> IT = dq.iterator();
         // while(IT.hasNext()){
-        //     System.out.println(IT.next());
+        // System.out.println(IT.next());
         // }
 
-        for(String name : dq){
+        for (String name : dq) {
             System.out.println(name);
         }
 
@@ -142,16 +137,17 @@ public class Main {
         set.add("Tree");
         set.add("Plant");
         set.add("Water");
-        System.out.println("Set: " + set);    
-
-
+        System.out.println("Set: " + set);
 
         HashSet<Student> set2 = new HashSet<>();
-        Student s1 = new Student("Yash",39,"A",19);
-        Student s2 = new Student("Tripti",37,"A",18);
-        Student s3 = new Student("Satkirat",27,"A",18);
-        Student s4 = new Student("Farhan",18,"A",18);
-        set2.add(s1) ; set2.add(s2); set2.add(s3); set2.add(s4);
+        Student s1 = new Student("Yash", 39, "A", 19);
+        Student s2 = new Student("Tripti", 37, "A", 18);
+        Student s3 = new Student("Satkirat", 27, "A", 18);
+        Student s4 = new Student("Farhan", 18, "A", 18);
+        set2.add(s1);
+        set2.add(s2);
+        set2.add(s3);
+        set2.add(s4);
 
         System.out.println("Set of Students : " + set2);
     }
